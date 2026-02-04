ca3ec24c993fcc830b0d5d462b38e8b453359318
Gustavo Cordeiro Galvao Van Erven <gvanerven@gmail.com>
2026-02-03 22:03:06 -0600

    Retrain

--- scripts/train_entrypoint.py
+++ scripts/train_entrypoint.py
@@ -30,7 +30,7 @@ def run_training():
     vocabulary_size = 32_768
     context_size = 1024
     tokenizer_name = f"tokenizers/custom/{vocabulary_size:_}"
-    model_name = f"Modern/large-classiccc-1024-unigram-32768-300ksteps"
+    model_name = f"Modern/large-classiccc-1024-unigram-32768-900ksteps"
 
     output_dir = f"training_test/{model_name}"
     os.makedirs(output_dir, exist_ok=True)
@@ -52,7 +52,6 @@ def run_training():
         reference_compile=False,
         attn_implementation="flash_attention_2",
         vocab_size=vocabulary_size,
-        #dtype=torch.float16,
         max_position_embeddings=1024,
     )
     config.vocab_size = vocabulary_size
@@ -69,7 +68,7 @@ def run_training():
 
     model = ModernBertForMaskedLM(config=config)
     # Reinitialize weights
-    model.apply(model._init_weights)
+    #model.apply(model._init_weights)
     # NOTE: We do NOT call model.to("cuda") or model.half().
     # The Trainer, powered by Accelerate, will handle device placement and mixed precision.
 
@@ -80,20 +79,16 @@ def run_training():
     training_args = TrainingArguments(
         output_dir=output_dir,
         overwrite_output_dir=False,
-        max_steps=300_000,
+        max_steps=900_000,
         per_device_train_batch_size=256,
-        gradient_accumulation_steps=2,
-        dataloader_num_workers=64,
+        gradient_accumulation_steps=1,
+        dataloader_num_workers=128,
         logging_strategy="steps",
         logging_first_step=True,
         logging_steps=1_000,
         save_strategy="steps",
         save_steps=1_000,
         save_total_limit=5,
-        optim="schedule_free_radam",
-        lr_scheduler_type="constant",
-        gradient_checkpointing=True,
-        learning_rate=5e-4,
         bf16=True,
         report_to="tensorboard",
     )
@@ -118,7 +113,7 @@ def run_training():
         trainer.train()
     accelerator.print("Training complete!")
 
-    trainer.save_model("saved_models/Modern/BERTomelo-ModernBERT-Large-300steps-1k")
+    trainer.save_model("saved_models/Modern/BERTomelo-ModernBERT-Large-900steps-1k")
 
 if __name__ == "__main__":
     run_training()
